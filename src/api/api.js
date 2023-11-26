fb01cbb05c59ceecd464bf5312e92091bdff8c36
damascus08 <yusophh@gmail.com>
2023-11-26 08:42:39 +0800

    update: api endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 export const fetchProducts = async () => {
     //consume provided api
+
+    const BASE_URL= process.env.REACT_APP_API_URL || 'https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com'
     try {
-        const response = await axios.get('https://3sb655pz3a.execute-api.ap-southeast-2.amazonaws.com/live/product');
+        const response = await axios.get(BASE_URL+'/live/product');
         return response.data;
     } catch (error) {
         console.error('Error fetching products:', error);
